84eee2334911d733eb0fb07922131ccd90c5c317
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2016-09-24 22:36:31 +0530

    Added tryAcquire method impl

--- jedis-lock-support/src/main/java/com/creativeneons/jedis/lock/SingleClusterJedisLockSupport.java
+++ jedis-lock-support/src/main/java/com/creativeneons/jedis/lock/SingleClusterJedisLockSupport.java
@@ -8,11 +8,13 @@ import redis.clients.jedis.JedisPool;
 
 /**
  * Single cluster lock support using Jedis
+ * 
  * @author Mazra, Gaurav Rai
  *
  */
 public class SingleClusterJedisLockSupport implements JedisLockSupport {
 
+	private static final long MIN_WAIT_MS = 100;
 	private JedisPool jedisPool;
 	private long lockAcquireTime;
 
@@ -34,11 +36,25 @@ public class SingleClusterJedisLockSupport implements JedisLockSupport {
 
 	@Override
 	public boolean tryAcquire(String key, String owner, long timeout, TimeUnit timeUnit) {
+		boolean acquired = false;
 		key = normalizeKey(key);
-		//TODO need implementation
-		return false;
+		long timeToWaitInMs = timeUnit == TimeUnit.MILLISECONDS ? timeout : timeUnit.toMillis(timeout);
+		try (Jedis jedis = jedisPool.getResource();) {
+			while (!acquired && timeToWaitInMs >= 0) {
+				timeToWaitInMs = timeToWaitInMs - MIN_WAIT_MS;
+				synchronized (key) {
+					acquired = isMine(key, owner, jedis) || added(key, owner, jedis);
+				}
+				try {
+					TimeUnit.MILLISECONDS.wait(MIN_WAIT_MS);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			}
+		}
+		return acquired;
 	}
-	
+
 	String lua_script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
 
 	@Override
@@ -47,7 +63,7 @@ public class SingleClusterJedisLockSupport implements JedisLockSupport {
 		synchronized (key) {
 			try (Jedis jedis = jedisPool.getResource();) {
 				Object result = jedis.eval(lua_script, 1, key, value(key, owner));
-				//TODO need to use logger abstraction possibly log4j
+				//TODO need to log it
 			}
 		}
 	}
@@ -56,11 +72,11 @@ public class SingleClusterJedisLockSupport implements JedisLockSupport {
 		String value = jedis.get(key);
 		return Objects.nonNull(value) && value.endsWith(owner);
 	}
-	
+
 	private String value(String key, String owner) {
 		return new StringBuilder(key.length() + owner.length() + 2).append(key).append("::").append(owner).toString();
 	}
-	
+
 	private boolean added(String key, String owner, Jedis jedis) {
 		return Objects.nonNull(setIfNotExists(key, owner, jedis));
 	}
